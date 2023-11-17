fe7d63d615341e5dbc80643a18adaabca7ba7ee9
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-11-17 14:52:39 -0400

    complete mobile response for about section

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// let tabLinks=document.getElementsByClassName("tab-links")
+// let tabContents=document.getElementsByClassName("tab-contents")
+// let open=document.getElementById("open-icon")
+// let close=document.getElementById("xmark")
+// open.addEventListener("click",openMenu)
+// close.addEventListener("click",closeMenu)
+
+// function openTab(tabName,evt){
+//   for(tabLink of tabLinks){
+//     tabLink.classList.remove("active-link")
+//   }
+//   for(tabContent of tabContents){
+//     tabContent.classList.remove("active-tab")
+//   }
+//   evt.currentTarget.classList.add("active-link")
+//   document.getElementById(tabName).classList.add("active-tab")
+// }
+
+// function openMenu(){
+//   sideMenu.style.right="0"
+// }
+
+// function closeMenu(){
+//   sideMenu.style.right="-200px"
+// }

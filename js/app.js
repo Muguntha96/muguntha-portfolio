d468b956ab24698f153bb111836be1e045ae7371
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-11-20 13:15:01 -0400

    resolve to bg to show the resume

--- js/app.js
+++ js/app.js
@@ -1,25 +1,13 @@
-// let tabLinks=document.getElementsByClassName("tab-links")
-// let tabContents=document.getElementsByClassName("tab-contents")
-// let open=document.getElementById("open-icon")
-// let close=document.getElementById("xmark")
-// open.addEventListener("click",openMenu)
-// close.addEventListener("click",closeMenu)
+document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('#sidemenu a');
 
-// function openTab(tabName,evt){
-//   for(tabLink of tabLinks){
-//     tabLink.classList.remove("active-link")
-//   }
-//   for(tabContent of tabContents){
-//     tabContent.classList.remove("active-tab")
-//   }
-//   evt.currentTarget.classList.add("active-link")
-//   document.getElementById(tabName).classList.add("active-tab")
-// }
-
-// function openMenu(){
-//   sideMenu.style.right="0"
-// }
-
-// function closeMenu(){
-//   sideMenu.style.right="-200px"
-// }
+  navLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+      const yOffset = targetSection.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({ top: yOffset, behavior: 'smooth' });
+    });
+  });
+});
